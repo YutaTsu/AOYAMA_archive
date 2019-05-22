2930f33e2190e2271500e368e93a4d2549cbda4e
YutaTsu <tsuney.762@gmail.com>
2019-05-22 17:05:58 +0900

    app

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,9 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-
-//kml読み込み
-viewer.dataSources.add(Cesium.KmlDataSource.load(''));
-
-
-//視点の変更
-viewer.zoomTo(viewer.entities);
+viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/hukui.kml"));
