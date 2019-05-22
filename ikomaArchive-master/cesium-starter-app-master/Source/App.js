8935800ff4b01b23e53f271985ad0b997e724df4
YutaTsu <tsuney.762@gmail.com>
2019-05-22 17:13:57 +0900

    app2

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/山陽堂書店.kml"));
+viewer.dataSources.add(Cesium.KmlDataSource.load("https://github.com/yutatsu/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/山陽堂書店.kml"));
