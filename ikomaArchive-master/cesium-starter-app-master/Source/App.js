730fd75b9d52663697786b2feafe414b1f14abc8
YutaTsu <tsuney.762@gmail.com>
2019-05-22 17:23:48 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load("https://github.com/YutaTsu/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/sanyodo.kml"));
+viewer.dataSources.add(Cesium.KmlDataSource.load("https://github.com/YutaTsu/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/Sanyodo.kml"));
