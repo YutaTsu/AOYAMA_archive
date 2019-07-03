e64c9c503be82edc6b47e5352b0c93f69d6828e2
YutaTsu <tsuney.762@gmail.com>
2019-07-03 20:41:49 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -2,13 +2,12 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/aoyama_localshops.kml"));
 
-var scene = viewer.scene;
-
-var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
-    Cesium.Cartesian3.fromDegrees(-75.62898254394531, 40.02804946899414, 0.0));
-var model = scene.primitives.add(Cesium.Model.fromGltf({
-    url : 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/aoyama_3D_practice.glb',
-    modelMatrix : modelMatrix,
-    scale : 200.0
-}));
  
+var viewer = new Cesium.Viewer('cesiumContainer');
+var entity = viewer.entities.add({
+    position : Cesium.Cartesian3.fromDegrees(-75.62898254394531, 40.02804946899414, 0.0),
+    model : {
+        uri : 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/aoyama_3D_practice.glb'
+    }
+});
+viewer.trackedEntity = entity;
