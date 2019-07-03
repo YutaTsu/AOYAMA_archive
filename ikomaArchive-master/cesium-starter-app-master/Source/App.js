25a884e659118fc692790f3f582d467a98193429
YutaTsu <tsuney.762@gmail.com>
2019-07-03 20:46:45 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -3,7 +3,6 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/aoyama_localshops.kml"));
 
  
-var viewer = new Cesium.Viewer('cesiumContainer');
 var entity = viewer.entities.add({
     position : Cesium.Cartesian3.fromDegrees(-75.62898254394531, 40.02804946899414, 0.0),
     model : {
