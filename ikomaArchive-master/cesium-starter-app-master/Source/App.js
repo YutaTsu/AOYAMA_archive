221f8165813995d65044b01e6768632456d3bac9
YutaTsu <tsuney.762@gmail.com>
2019-07-03 16:57:16 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,3 +1,13 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/aoyama_localshops.kml"));
+
+var scene = viewer.scene;
+
+var modelMatrix + Cesium.Transforms.eastNorthUpToFixesFrame{
+    Cesium.Cartesian3.fromDegrees(-75.628982543934531, 40.02804946899414, 0.0)};
+var model = scene.primitives.add(Cesium.Model.fromDae({
+    url : 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/aoyama_3d_practice.dae',
+    modelMatrix : modelMatrix,
+    acale : 200.0
+}));
