0b397dd3646fc2e37b42bcf62749fe3736274214
YutaTsu <tsuney.762@gmail.com>
2019-07-04 18:08:12 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -6,8 +6,9 @@ viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYA
 var entity = viewer.entities.add({
     position : Cesium.Cartesian3.fromDegrees(-75.62898254394531, 40.02804946899414, 0.0),
     model : {
-        uri : 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/aoyama_3D_practice.glb'
+    uri : 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/aoyama_3D_practice.glb'
     }
 });
 
 viewer.trackedEntity = entity;
+
