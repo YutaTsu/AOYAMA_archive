ed98248ba870578eb779082a70c812fcb35d7cb7
YutaTsu <tsuney.762@gmail.com>
2019-07-06 14:33:56 +0900

    a

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -7,7 +7,7 @@ var scene = viewer.scene;
 var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
     Cesium.Cartesian3.fromDegrees(-75.62898254394531, 40.02804946899414, 0.0));
 var model = scene.primitives.add(Cesium.Model.fromGltf({
-    url: 'https://github.com/YutaTsu/AOYAMA_archive/blob/master/ikomaArchive-master/cesium-starter-app-master/aoyama_3D_practice.gltf',
+    url: 'https://github.com/YutaTsu/AOYAMA_archive/blob/master/ikomaArchive-master/cesium-starter-app-master/Source/aoyama_3D_practice.gltf',
     modelMatrix : modelMatrix,
     scale : 200.0
 }));
