017b44a42cadbe1d40930478045720c1af58ca4b
YutaTsu <tsuney.762@gmail.com>
2019-07-06 18:27:22 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,7 +1,7 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 var entity = viewer.entities.add({
     position : Cesium.Cartesian3.fromDegrees(-139.7124052048, 35.66575165403),
-    model : {'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/aoyama_3D_practice.glb'
+    model : {'https://github.com/AnalyticalGraphicsInc/cesium/blob/master/Apps/SampleData/models/GroundVehicle/GroundVehicle.glb'
     }
 });
 viewer.trackedEntity = entity;
