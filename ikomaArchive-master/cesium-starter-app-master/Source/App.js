7e84576c1d9b4f6c1caabe8c8cd7276bbe3bae5f
YutaTsu <tsuney.762@gmail.com>
2019-07-06 18:28:51 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,7 +1,8 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 var entity = viewer.entities.add({
-    position : Cesium.Cartesian3.fromDegrees(-139.7124052048, 35.66575165403),
-    model : {'https://github.com/AnalyticalGraphicsInc/cesium/blob/master/Apps/SampleData/models/GroundVehicle/GroundVehicle.glb'
+    position : Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706),
+    model : {
+        uri : '../../../../Apps/SampleData/models/GroundVehicle/GroundVehicle.glb'
     }
 });
 viewer.trackedEntity = entity;
