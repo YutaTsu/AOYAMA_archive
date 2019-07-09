da594563209e40af28de775b9584b962e1a246df
YutaTsu <tsuney.762@gmail.com>
2019-07-09 19:32:42 +0900

    a

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -3,8 +3,11 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 var entity = viewer.entities.add({
     position: Cesium.Cartesian3.fromDegrees(35.665181, 139.712565),
     model: {
-        uri: 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/modeltry.glb'
+        uri: 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/modeltry.gltf'
     }
 });
+
+viewer.camera.flyTo({   
+     destination : Cesium.Cartesian3.fromDegrees(35.665181, 139.712565, 2000.0)});
      
      
