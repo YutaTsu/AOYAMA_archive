e8cec55c256db09be16ec0b5235a7d8edbfe69d8
YutaTsu <tsuney.762@gmail.com>
2019-07-09 21:22:06 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,12 +1,10 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
-    Cesium.Cartesian3.fromDegrees(35.665181,139.712565, 0.0));
+    Cesium.Cartesian3.fromDegrees(139.712565,35.665181, 0.0));
 var model = viewer.entities.add(Cesium.Model.fromGltf({
     url : 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/modeltry2.gltf',
-    modelMatrix : modelMatrix,
-    scale : 200.0
 }));
 
 viewer.camera.flyTo({   
-     destination : Cesium.Cartesian3.fromDegrees(35.665181, 139.712565, 2000.0)}); 
+     destination : Cesium.Cartesian3.fromDegrees(139.712565,35.665181, 2000.0)}); 
