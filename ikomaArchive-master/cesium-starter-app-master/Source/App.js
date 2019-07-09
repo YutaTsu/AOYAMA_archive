279e59d92dacc184813c00863cdcf8269981a3ce
YutaTsu <tsuney.762@gmail.com>
2019-07-09 19:36:52 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -3,7 +3,7 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 var entity = viewer.entities.add({
     position: Cesium.Cartesian3.fromDegrees(35.665181, 139.712565),
     model: {
-        uri: 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/modeltry.gltf'
+        uri: 'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/modeltry.gltf'
     }
 });
 
