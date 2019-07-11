9519b6039133a70dbf950eccf6675e7dbc7b1318
YutaTsu <tsuney.762@gmail.com>
2019-07-11 17:50:29 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,12 +1,19 @@
-var viewer = new Cesium.Viewer('cesiumContainer')
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5NmU4OThjZi0xNjRmLTRjZWYtYmE0Yy03MDM2NzdmZjFjOTMiLCJpZCI6MTMwMzcsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjI0MDc0ODN9.S-viGfl3AwKBpxPIz-mSfYtMyRUSTD5keEqfjtVYK9M';
+
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider : new Cesium.CesiumTerrainProvider({
+        url: Cesium.IonResource.fromAssetId(1)
+    })
+});
+viewer.scene.globe.depthTestAgainstTerrain = true;
 
 var tileset = viewer.scene.primitives.add(
     new Cesium.Cesium3DTileset({
         url: Cesium.IonResource.fromAssetId(33964)
     })
 );
-viewer.camera.flyTo({   
-     destination : Cesium.Cartesian3.fromDegrees(139.712565,35.665181, 2000.0)}); 
-     
-     
-     
+
+viewer.zoomTo(tileset)
+    .otherwise(function (error) {
+        console.log(error);
+    });
