ce42174e0183d1af73de7c8d2dd0a97c2d9d10f6
YutaTsu <tsuney.762@gmail.com>
2019-07-11 14:59:41 +0900

    a

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -2,7 +2,7 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 var entity = viewer.entities.add({
     position : Cesium.Cartesian3.fromDegrees(139.712565,35.665181, 500),
     model : {
-        url :'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/model.gltf'
+        url :'https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/aoyama_model.kml'
     }
 });
 viewer.trackedEntity = entity;
