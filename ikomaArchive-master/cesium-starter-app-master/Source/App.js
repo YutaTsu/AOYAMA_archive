961d4a980bdbd045ca9b7f3695b57132fd62f485
YutaTsu <tsuney.762@gmail.com>
2019-07-11 17:55:48 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,5 +1,3 @@
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5NmU4OThjZi0xNjRmLTRjZWYtYmE0Yy03MDM2NzdmZjFjOTMiLCJpZCI6MTMwMzcsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjI0MDc0ODN9.S-viGfl3AwKBpxPIz-mSfYtMyRUSTD5keEqfjtVYK9M';
-
 var viewer = new Cesium.Viewer('cesiumContainer', {
     terrainProvider : new Cesium.CesiumTerrainProvider({
         url: Cesium.IonResource.fromAssetId(1)
