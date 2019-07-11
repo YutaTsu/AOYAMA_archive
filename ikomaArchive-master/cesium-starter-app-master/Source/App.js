bffbf4dab9067ed501450f96c7f1221f67902774
YutaTsu <tsuney.762@gmail.com>
2019-07-11 15:04:51 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,6 +1,5 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/aoyama_model.kml"));
-viewer.trackedEntity = entity;
 viewer.camera.flyTo({   
      destination : Cesium.Cartesian3.fromDegrees(139.712565,35.665181, 2000.0)}); 
      
