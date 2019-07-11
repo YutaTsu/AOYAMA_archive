98a6787a1533a20b034808289425062dc6e23c1f
YutaTsu <tsuney.762@gmail.com>
2019-07-11 17:00:21 +0900

    Update App.js

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,4 +1,10 @@
-var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider : new Cesium.CesiumTerrainProvider({
+         url: Cesium.IonResource.fromAssetId(1)
+         })
+});
+viewer.scene.globe.depthTestAgainstTerrain = true;
+
 var tileset = viewer.scene.primitives.add(
     new Cesium.Cesium3DTileset({
         url: Cesium.IonResource.fromAssetId(33964)
