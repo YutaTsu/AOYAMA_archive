9560b151a66ec40ca9a13ae44a3fca4dad6a6a22
YutaTsu <tsuney.762@gmail.com>
2019-11-30 23:54:36 +0900

    a

--- ikomaArchive-master/cesium-starter-app-master/Source/App.js
+++ ikomaArchive-master/cesium-starter-app-master/Source/App.js
@@ -1,3 +1,1 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
-viewer.dataSources.add(Cesium.KmlDataSource.load("https://yutatsu.github.io/AOYAMA_archive/ikomaArchive-master/cesium-starter-app-master/Source/aoyama_localshops.kml"));
-viewer.camera.flyTo({destination : Cesium.Cartesian3.fromDegrees(139.712533,35.665439,1000)});
